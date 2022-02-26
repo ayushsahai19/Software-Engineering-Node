411f8ada9b6b97925a49db2111e935b25c42ed77
ayushsahai19 <ayushsahai96@gmail.com>
2022-02-25 20:16:58 -0500

    Assignment 2

--- mongoose/messages/MessageModel.ts
+++ mongoose/messages/MessageModel.ts
@@ -1,3 +1,7 @@
+/**
+ * @file Implements mongoose model to CRUD
+ * documents in the likes collection
+ */
 import mongoose from "mongoose";
 import MessageSchema from "./MessageSchema";
 const MessageModel = mongoose.model("MessageModel", MessageSchema);
